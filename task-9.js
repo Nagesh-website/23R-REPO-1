7505d2b73f979694a70d17cf0975d67cba46373b
Nagesh-website <sagarnagesh685@gmail.com>
2024-10-28 20:47:53 +0530

    adding js task-9

---
+++ task-9.js
@@ -0,0 +1,36 @@
+var age1 =18;
+var citizenship=true;
+if(age1>=18 && citizenship ==true){
+    console.log("Can Vote")
+}
+else{
+    console.log("Can't Vote ")
+}
+
+var age2 =18;
+var invitation = true;
+if(age2 >=21 && invitation == true)
+    console.log("You Can Ente the Club")
+else{
+    console.log("You can Not Enter The Club")
+}
+
+
+var memebership = true;
+age3 = 75;
+if(memebership!==false && age3>=61)
+    console.log("isEligibile for discount")
+else {
+    console.log("is not eligible for the dicount")
+}
+
+
+var cgpa=8.5
+var extracarricular=true
+var recommendation=false
+if(cgpa>=3.5 && extracarricular==true || recommendation==false){
+    console.log("is eligible for scholarship")
+
+}else{
+    console.log("not eligible for scholarship")
+}
